14ed59562cc2d3024694c1ba9eb24b831651a5f0
Emerson <paraqvaria.it.4@gmail.com>
2023-03-02 07:17:54 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+    <h1>HOLA MUNDO FROM AWS</h1>
+    `)
+})
+
+app.listen(8080, () => {
+    console.log('Running on port 8080');
+});
