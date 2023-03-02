bbdcab44694fafc0a0869290f0f982f10df38688
Emerson <paraqvaria.it.4@gmail.com>
2023-03-02 07:39:27 -0300

    TEST

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ app.get('/', (req, res) => {
     res.send(`
     <h1>HOLA MUNDO FROM AWS</h1>
     `)
-})
+});
+
+app.get('/test', (req, res) => {
+    res.send(`
+    <h1>ES UN NUEVO DIA TEST</h1>
+    `)
+});
 
 app.listen(8080, () => {
     console.log('Running on port 8080');
